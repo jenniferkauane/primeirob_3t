3b582334ce1b3c8eb3997d1dd687d45b6aace7d2
jenniferkauane <110115877+jenniferkauane@users.noreply.github.com>
2022-11-09 12:47:46 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -116,3 +116,29 @@ function soma(){
 
 }
 
+
+function calcularaiz(){
+
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    let c = document.getElementById("c").value;
+    //ax2 + bx + c = 0
+
+    let delta = (b*b) - (4*a*c);
+
+    if (delta < 0){
+        document.getElementById("raiz").innerHTML = "Nao possui raiz real" ;
+    } else if (delta == 0){
+        let x1 = (-b)/(2*a);
+        document.getElementById("raiz").innerHTML = "x1 = x2 = " + x1;
+    } else {
+        let x1 = ((-b) + Math.sqrt(delta))/(2*a);
+        let x2 = ((-b) - Math.sqrt(delta))/(2*a);
+        document.getElementById("raiz").innerHTML = "x1 = " + x1 + " x2 = " + x2;
+
+    }
+
+}
+
+
+
